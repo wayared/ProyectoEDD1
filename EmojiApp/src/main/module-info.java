17ff44efb9d0d633e28901fd009976378ac10dbf
Jared <jarecast@espol.edu.ec>
2023-06-15 00:30:06 -0500

    module-info added and scene builder first steps implemented

---
+++ EmojiApp/src/main/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module EmojiApp {
+    requires javafx.swt;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+}
